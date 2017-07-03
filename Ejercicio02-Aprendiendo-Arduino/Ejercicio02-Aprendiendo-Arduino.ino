45ec1e5de23b03443962b434ec4a7b0d3e9267ad
Enrique Crespo <jecrespom@gmail.com>
2017-07-03 02:08:08 +0200

    Otra opción de hacerlo

--- Ejercicio02-Aprendiendo-Arduino/Ejercicio02-Aprendiendo-Arduino.ino
+++ Ejercicio02-Aprendiendo-Arduino/Ejercicio02-Aprendiendo-Arduino.ino
@@ -1,1 +1,22 @@
-Prueba
+#define PIN_LDR A1
+
+int maximo = 0;
+int minimo = 1023;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(PIN_LDR);
+  if (sensorValue > maximo)
+    maximo = sensorValue;
+  if (sensorValue < minimo)
+    minimo = sensorValue;
+  Serial.print(sensorValue);
+  Serial.print(",");
+  Serial.print(maximo);
+  Serial.print(",");
+  Serial.println(minimo);
+  delay(500);
+}
