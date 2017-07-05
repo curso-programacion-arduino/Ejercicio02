cc4de7c59598a568c2cd9bc83c080796565fd5f3
isoriaa <isoria@fttn.es>
2017-07-05 19:42:41 +0200

    con maximo y minimo

--- Ejercicio02-Iv_n/Ejercicio02-Iv_n.ino
+++ Ejercicio02-Iv_n/Ejercicio02-Iv_n.ino
@@ -1,3 +1,6 @@
+int maximo = 0;
+int minimo = 1023;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -7,6 +10,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   int lectura;
   lectura = analogRead(A1);
-  Serial.println(lectura);
-  delay(500);
+  
+  if (lectura<minimo){
+    minimo=lectura;
+  }
+  if (lectura>maximo){
+    maximo=lectura;
+  }
+  Serial.print(lectura);
+  Serial.print(',');
+  Serial.print(maximo);
+  Serial.print(',');
+  Serial.println(minimo);
+  delay(50);
 }
